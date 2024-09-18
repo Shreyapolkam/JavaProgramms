08598f66e0e8a4ae44d386450c82c153abe13411
Shreyapolkam <shreya.polkam331@gmail.com>
2024-09-18 14:30:51 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,8 @@
+
+public class Demo{//class declaration
+
+       public static void main(String[] args){ //main method
+	   
+	          System.out.println("HELLO WORLD"); // print the statement
+	   }
+}
